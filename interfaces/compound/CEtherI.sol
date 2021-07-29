039683f70681fbddb69d60d8468547021bd96cf4
jmonteer <jmonteer@protonmail.com>
2021-07-29 18:21:26 +0200

    fix: compiling

---
+++ interfaces/compound/CEtherI.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.6.12;
+
+import "./CTokenI.sol";
+
+interface CEtherI is CTokenI {
+    function redeemUnderlying(uint256 redeemAmount) external returns (uint256);
+
+    function redeem(uint256 redeemTokens) external returns (uint256);
+
+    function liquidateBorrow(address borrower, CTokenI cTokenCollateral) external payable;
+
+    function borrow(uint256 borrowAmount) external returns (uint);
+
+    function mint() external payable;
+    function repayBorrow() external payable;
+}
